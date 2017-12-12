02a1cc823f6a94fbf6240a4283fe4dfb7b71f19f
Chris Conley <chris.m.conley@gmail.com>
2017-12-12 11:45:03 -0800

    Finished MaxHeap implementation

--- heap.py
+++ heap.py
@@ -1,29 +1,49 @@
 class MaxHeap:
     def __init__(self, keys=[]):
         self._size = 0
+        self._heap = [None]
         for key in keys:
             self.insert(key)
 
     def is_empty(self):
         return self._size == 0
 
+    @property
     def size(self):
-        pass
+        return self._size
 
     def insert(self, value):
         self._size += 1
+        self._heap.append(value)
+        self._swim(self._size)
 
     def del_max(self):
-        pass
+        max_key = self._heap[1]
+        self._exchange(1, self._size)
+        self._size -= 1
+        self._heap.pop()
+        self._sink(1)
+        return max_key
 
     def _less(self, i, j):
-        pass
+        return self._heap[i] < self._heap[j]
 
     def _exchange(self, i, j):
-        pass
+        key = self._heap[i]
+        self._heap[i] = self._heap[j]
+        self._heap[j] = key
 
     def _swim(self, k):
-        pass
+        while k > 1 and self._less(int(k/2), k):
+            self._exchange(int(k/2), k)
+            k = int(k/2)
 
     def _sink(self, k):
-        pass
+        while 2 * k <= self._size:
+            j = 2 * k
+            if j < self._size and self._less(j, j+1):
+                j += 1
+            if not self._less(k, j):
+                break
+            self._exchange(k, j)
+            k = j
--- test/test_heap.py
+++ test/test_heap.py
@@ -10,3 +10,30 @@ class MaxHeapTests(unittest.TestCase):
 
         heap = MaxHeap([1, 2])
         self.assertFalse(heap.is_empty())
+
+    def test_size_after_insertion(self):
+        heap = MaxHeap([1, 2])
+        heap.insert(3)
+        heap.insert(4)
+        self.assertEqual(heap.size, 4)
+
+    def test_insert_and_del_max(self):
+        heap = MaxHeap([1, 2])
+        heap.insert(4)
+        heap.insert(3)
+
+        self.assertEqual(heap.size, 4)
+
+        self.assertEqual(heap.del_max(), 4)
+        self.assertEqual(heap.del_max(), 3)
+        self.assertEqual(heap.del_max(), 2)
+        self.assertEqual(heap.del_max(), 1)
+
+    def test_insert_and_del_max_from_algs_book(self):
+        heap = MaxHeap(['E', 'P', 'I', 'S', 'N', 'H', 'G', 'R', 'O', 'A'])
+        heap.insert('T')
+
+        self.assertEqual(heap.del_max(), 'T')
+        self.assertEqual(heap.del_max(), 'S')
+        self.assertEqual(heap.del_max(), 'R')
+        self.assertEqual(heap.del_max(), 'P')
